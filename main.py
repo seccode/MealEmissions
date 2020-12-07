60d8ab5c8245cf64ad675bdd96f712fc751a1225
seccode <sec288@cornell.edu>
2020-12-07 16:25:02 -0500

    meeting

--- main.py
+++ main.py
@@ -46,7 +46,6 @@ def get_food_loss_and_waste_rates():
     """
     return pd.read_excel("food_loss_and_waste_rates.xlsx", index_col=0)
 
-
 # Load data
 SALMON_MEAL_KIT_INGREDIENTS, CHEESEBURGER_MEAL_KIT_INGREDIENTS = \
     get_meal_kit_ingredients()
@@ -64,21 +63,21 @@ def generate_parameters():
     Generate random variable parameters for simulation
     """
     return {
-        "R": 0.1,
-        "D_TM": 976.87,
-        "C_T": 0.28 / 10**6,
-        "Y": 10,
-        "C_I": 0.28,
-        "N_M": 3,
-        "D_TG": 47.15,
-        "H_DF": 48.5,
-        "H_WF": 18.23,
-        "C_D": 6.62 / 10**6,
-        "C_A": 6.44 / 10**6,
-        "D_L": 4.43,
-        "V": 23.36,
-        "C_G": 0.28,
-        "N_G": 5
+        "R": 0.1, # Home waste rate
+        "D_TM": 796.87, # Meal Kit transportation distance
+        "C_T": 0.28 / 10**6, # Fuel emissions
+        "Y": 10, # Energy consumed per package
+        "C_I": 0.28, # Fuel emissions
+        "N_M": 3, # Number of meals per kit
+        "D_TG": 47.15, # Grocery transportation distance
+        "H_DF": 48.5, # Grocery display time
+        "H_WF": 18.23, # Hours in walk-in cooler
+        "C_D": 6.62 / 10**6, # Display emissions
+        "C_A": 6.44 / 10**6, # Walk-in cooler emissions
+        "D_L": 4.43, # Grocery last-mile distance
+        "V": 23.36, # Vehicle fuel efficiency
+        "C_G": 0.28, # Fuel emissions
+        "N_G": 5 # Number of grocery meals per trip
     }
 
 class Meal:
@@ -97,10 +96,7 @@ class MealKitService:
         Q_MF = [e + u for e, u in zip(meal.meal_kit_ingredients["Food Eaten (g)"],
             meal.meal_kit_ingredients["Food Unused (g)"])]
 
-        # TODO, add parameter to Monte Carlo params
-        R = 0.1
-
-        self.Q_CF = [q / (1 - R) for q in Q_MF]
+        self.Q_CF = [q / (1 - self.params["R"]) for q in Q_MF]
         
         self.C_F = [FOOD_AND_PACKAGING_EMISSIONS.loc[item]["kg CO2-eq/g "] for item in \
             meal.meal_kit_ingredients.index]
@@ -144,9 +140,18 @@ class MealKitService:
     def get_last_mile_transportation_emissions(self):
 
         ret = self.params["Y"] * self.params["C_I"] / self.params["N_M"]
-        print("Meal kit delivery emissions: {}".format(ret))
+        print("Meal kit last mile emissions: {}".format(ret))
 
         return ret
+
+    def get_individual_emissions(self):
+        return (
+            self.get_production_emissions(),
+            self.get_packaging_emissions(),
+            self.get_processing_emissions(),
+            self.get_delivery_emissions(),
+            self.get_last_mile_transportation_emissions()
+        )
 
     def get_total_emissions(self):
         return self.get_production_emissions() + \
@@ -216,6 +221,15 @@ class GroceryService:
 
         return ret
 
+    def get_individual_emissions(self):
+        return (
+            self.get_production_emissions(),
+            self.get_packaging_emissions(),
+            self.get_transportation_emissions(),
+            self.get_retail_operation_emissions(),
+            self.get_last_mile_transportation_emissions()
+        )
+
     def get_total_emissions(self):
         return self.get_production_emissions() + \
             self.get_packaging_emissions() + \
@@ -226,6 +240,7 @@ class GroceryService:
 
 def main():
 
+    # Create meals
     meal_1 = Meal(
         "Salmon",
         SALMON_MEAL_KIT_INGREDIENTS,
@@ -238,13 +253,16 @@ def main():
         CHEESEBURGER_GROCERY_MEAL_INGREDIENTS
         )
     
+    # Get random variable values
     params = generate_parameters()
 
+    # Instantiate meal services
     mks_1 = MealKitService(meal_1, params)
     mks_2 = MealKitService(meal_2, params)
     gs_1 = GroceryService(meal_1, params)
     gs_2 = GroceryService(meal_2, params)
 
+    # Print total emissions for each meal service
     for mk, gs in zip([mks_1, mks_2], [gs_1, gs_2]):
         print("Meal Kit Service Total Emissions for {} meal: {} kg CO2\n".format(
             mk.meal.name, mk.get_total_emissions()))
